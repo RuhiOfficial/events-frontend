48990a47b313e79ec38cec26287d708c26dea0a1
RuhiOfficial <ruhiequasar9@gmail.com>
2023-12-02 23:53:22 +0530

    event modal designing

---
+++ src/components/ImageUploader/index.js
@@ -0,0 +1,73 @@
+// ImageUploader.js
+import React, { useState } from 'react';
+import { FiCamera } from 'react-icons/fi';
+
+const ImageUploader = () => {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        setImage(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <style>
+        {`
+          /* Your custom styles for the camera button */
+          .camera-button {
+            
+            width: 100px;
+            height: 100px;
+            
+        
+    
+            cursor: pointer;
+          }
+
+          .camera-button img {
+            width:150px;
+            height:150px;
+            object-fit: cover;
+          }
+
+          
+
+          /* Hide input type file */
+          .hidden-input {
+            display: none;
+          }
+        `}
+      </style>
+
+      <label htmlFor="fileInput" className="camera-button">
+        {image ? (
+          <img src={image} alt="Uploaded" />
+        ) : (
+          <div className="camera-icon">
+            {/* Use the FiCamera icon from react-icons */}
+            <FiCamera size={150} color="#ffffff" />
+          </div>
+        )}
+        <input
+          id="fileInput"
+          type="file"
+          className="hidden-input"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
+      </label>
+    </div>
+  );
+};
+
+export default ImageUploader;
