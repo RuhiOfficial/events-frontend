4e2251e07eaef79a85baba853504cd6ad8f3f444
Ruhani9 <ruhiequasar9@gmail.com>
2024-02-07 12:17:54 +0530

    Time picker and build

--- src/components/Timepicker/index.js
+++ src/components/Timepicker/index.js
@@ -3,20 +3,40 @@ import TimePicker from 'rc-time-picker';
 import 'rc-time-picker/assets/index.css';
 import moment from 'moment';
 
-const Timepicker = ({ value, onChange, placeholder,className }) => {
-  // Convert string value to moment object
-  
+const Timepicker = ({ value, onChange, placeholder, className }) => {
+  const [timePickerVisible, setTimePickerVisible] = useState(false);
+
+  const handleTimeChange = (time) => {
+    // Call the onChange callback with the selected time
+    onChange(time);
+
+    // Close the TimePicker popup
+    setTimePickerVisible(false);
+  };
+
+  const handleTimePickerBlur = () => {
+    // Close the TimePicker when it loses focus
+    setTimePickerVisible(false);
+  };
 
   return (
-    <TimePicker
-      onChange={onChange}
-      value={value ? moment(value) : null}
-      showSecond={false}
-      format="h:mm A"
-      use12Hours
-      placeholder={placeholder}
-      className={`custom-timepicker ${className}`}
-    />
+    <div
+      onMouseEnter={() => setTimePickerVisible(true)}
+      onMouseLeave={() => setTimePickerVisible(false)}
+    >
+      <TimePicker
+        onChange={handleTimeChange}
+        onBlur={handleTimePickerBlur}
+        onOpen={() => setTimePickerVisible(true)}
+        value={value ? moment(value) : null}
+        showSecond={false}
+        format="h:mm A"
+        use12Hours
+        placeholder={placeholder}
+        className={`custom-timepicker ${className}`}
+        open={timePickerVisible}
+      />
+    </div>
   );
 };
 
